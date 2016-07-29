8b6d45cd720ba365b8bf1d7a0d3611a00ab30ad1
Yanze LIU <yanzeliu.france@gmail.com>
2016-07-29 14:56:54 -0700

    edit /etc/hotsts to make the social auth work

--- bookmarks/settings.py
+++ bookmarks/settings.py
@@ -46,6 +46,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
     'account',
+    'social.apps.django_app.default',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
